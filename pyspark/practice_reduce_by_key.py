3b6e321a35caf79bae05d8248eb0bf11b4addd6f
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-11-29 21:18:31 +0330

    Update practice_reduce_by_key.py to become better

--- pyspark/practice_reduce_by_key.py
+++ pyspark/practice_reduce_by_key.py
@@ -15,9 +15,7 @@ def main():
     rdd = rdd.reduceByKey(lambda x, y: x + y)
     rdd = rdd.filter(lambda x: x[1] > 1)
 
-    rdd = rdd.map(lambda x: (x[0].split(" ")[0], x[1]))
-    rdd = rdd.map(lambda x: (x[0], 1))
-
+    rdd = rdd.map(lambda x: (x[0].split(" ")[0], 1))
     rdd = rdd.reduceByKey(lambda x, y: x + y)
 
     result = rdd.collect()
