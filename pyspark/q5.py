5a3b0be8747739c900cbfab9b296ff6b8c7b5a97
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-11-30 16:45:58 +0330

    Add q5.py

---
+++ pyspark/q5.py
@@ -0,0 +1,38 @@
+"""average views of rate >= 3 and rate < 3
+
+0  | 1        | 2   | 3        | 4      | 5     | 6    | 7       | 8        | 9
+id | uploader | age | category | length | views | rate | ratings | comments | related_ids
+
+"""
+
+from pyspark import SparkContext
+
+
+def main():
+    """main function"""
+
+    sc = SparkContext("local", "q5")
+
+    # prepare data
+    file_path = "data/youtube.txt"
+    rdd = sc.textFile(file_path)
+    rdd = rdd.map(lambda x: x.split("\t"))
+    rdd = rdd.filter(lambda x: len(x) > 6)
+
+    rdd_1 = rdd.filter(lambda x: x[3].strip() == "Entertainment")
+    rdd_1 = rdd_1.filter(lambda x: float(x[2]) > 90)
+    rdd_1 = rdd_1.map(lambda x: float(x[6]))
+    result_1 = rdd_1.max()
+
+    print(result_1)
+
+    rdd_2 = rdd.map(lambda x: (int(x[5]), x[1]))
+
+    result_2 = rdd_2.top(10)
+
+    for x in result_2:
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
