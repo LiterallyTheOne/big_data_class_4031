d50276edb29ac071dee7f7b4a0df468c72c66b76
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-12-06 16:35:46 +0330

    add p6_value_more_than_one.py

---
+++ pyspark/p6_value_more_than_one.py
@@ -0,0 +1,40 @@
+"""
+
+
+0  | 1        | 2   | 3        | 4      | 5     | 6    | 7       | 8        | 9
+id | uploader | age | category | length | views | rate | ratings | comments | related_ids
+
+"""
+
+from pyspark import SparkContext
+
+
+def main():
+    """main function"""
+
+    sc = SparkContext("local", "p6")
+
+    # prepare data
+    file_path = "pyspark/example_3.txt"
+    rdd = sc.textFile(file_path)
+    rdd = rdd.map(lambda x: x.split("\t"))
+    rdd = rdd.filter(lambda x: len(x) > 1)
+
+    rdd = rdd.map(
+        lambda x: (x[0], (int(x[1]), int(x[2]))),
+    )
+
+    rdd = rdd.repartition(2)
+
+    rdd = rdd.reduceByKey(
+        lambda x, y: (x[0] + y[0], x[1] + y[1]),
+    )
+
+    result = rdd.collect()
+
+    for x in result:
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
